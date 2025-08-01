955c7362e5717faa003c604a3bf8f54e7a94871e
How Bizarre <howbizarre@gmail.com>
2025-08-01 17:45:57 +0300

    fix(seo): update SEO data source and restructure content configuration

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 const { t, locale } = useI18n();
 
-const { data: seo } = await useLazyAsyncData('content-landing-seo', () => {
-  return queryCollection('landing').where('stem', '=', 'landing/seo').first();
+const { data: seo } = await useLazyAsyncData('content-seo-home', () => {
+  return queryCollection('seo').where('stem', '=', 'seo/home').first();
 });
 
 const title = computed(() => seo.value?.content.title[locale.value]);
@@ -21,6 +21,7 @@ useSeoMeta({
     <h1 class="text-4xl font-bold">{{ t('THE_QUESTION') }}</h1>
     <ButtonColorMode />
     <ButtonLangSwitcher />
+
     <pre>{{ seo?.content.description[locale] }}</pre>
   </div>
 </template>
--- content.config.ts
+++ content.config.ts
@@ -2,9 +2,9 @@ import { defineContentConfig, defineCollection, z } from '@nuxt/content';
 
 export default defineContentConfig({
   collections: {
-    landing: defineCollection({
+    seo: defineCollection({
       type: 'data',
-      source: 'landing/**.json',
+      source: 'seo/**.json',
       schema: z.object({
         'content': z.object({
           title: z.object({ 'bg': z.string(), 'en': z.string() }),
