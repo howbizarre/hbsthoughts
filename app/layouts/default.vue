870a3504035e4317212a75e98cb0e73039a87537
How Bizarre <howbizarre@gmail.com>
2025-08-02 00:13:43 +0300

    feat: enhance SEO and layout structure with i18n support and improved styling

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-    <UContainer>
+  <UContainer>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/help">Help</NuxtLink>
     <ButtonColorMode />
     <ButtonLangSwitcher />
 
-      <main class="flex-1">
-        <slot />
-      </main>
+    <main class="flex-1">
+      <slot />
+    </main>
   </UContainer>
 </template>
