cec3b29135c31f56aa66a4d0471042435b2b892e
How Bizarre <howbizarre@gmail.com>
2025-08-01 21:40:56 +0300

    feat(layout): add default layout with navigation and theme switcher feat(app): integrate NuxtRouteAnnouncer and restructure app template fix(index): remove unused ButtonColorMode and ButtonLangSwitcher components

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <UApp>
-    <NuxtPage />
+    <NuxtRouteAnnouncer />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </UApp>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <UContainer>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/help">Help</NuxtLink>
+    <ButtonColorMode />
+    <ButtonLangSwitcher />
+    <main>
+      <slot />
+    </main>
+  </UContainer>
+</template>
