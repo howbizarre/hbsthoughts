4924806ea351343873cb95ff2b56cfe6fb99e318
How Bizarre <howbizarre@gmail.com>
2025-08-01 15:33:46 +0300

    feat: add ColorMode and LangSwitcher components, implement sticky header, and enhance footer with social links

---
+++ app/components/button/LangSwitcher.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { locale, locales } = useI18n();
+const availableLocales = computed(() => (locales.value).filter(i => i.code !== locale.value));
+
+const switchLocalePath = useSwitchLocalePath();
+
+watch(locale, () => useHead({ htmlAttrs: { lang: locale.value } }));
+</script>
+
+<template>
+  <template v-for="locale in availableLocales" :key="locale.code">
+    <UButton :to="switchLocalePath(locale.code)"
+             icon="i-heroicons-language"
+             size="md"
+             color="neutral"
+             variant="ghost">
+      {{ locale.name }}
+    </UButton>
+  </template>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,5 +5,7 @@ const { t } = useI18n();
 <template>
   <div>
     <h1 class="text-4xl font-bold">{{ t('THE_QUESTION') }}</h1>
+    <ButtonColorMode />
+    <ButtonLangSwitcher />
   </div>
 </template>
