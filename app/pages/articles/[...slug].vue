82f55e566f0a7b3e2a42dc6505ca20113f50f4b2
How Bizarre <howbizarre@gmail.com>
2025-08-04 17:37:54 +0300

    feat: enhance article page with surrounding articles navigation and improved hr styling

--- app/pages/articles/[...slug].vue
+++ app/pages/articles/[...slug].vue
@@ -3,10 +3,23 @@ const { locale } = useI18n();
 const route = useRoute();
 const slug = ref(route.params.slug);
 
-const { data: article } = await useLazyAsyncData(route.path, () => {
+const { data: article } = await useLazyAsyncData(route.path, async () => {
   const collectionName = `articles_${locale.value}` as 'articles_bg' | 'articles_en';
-  return queryCollection(`${collectionName}`).path(`/articles/${locale.value}/${slug.value}`).first()
+  return await queryCollection(`${collectionName}`).path(`/articles/${locale.value}/${slug.value}`).first()
 }, { server: true });
+
+const { data: surroundingArticles } = await useLazyAsyncData(
+  () => `surround-article-${route.path}-${locale.value}`, // реактивен ключ
+  async () => {
+    const collectionName = `articles_${locale.value}` as 'articles_bg' | 'articles_en';
+    return await queryCollectionItemSurroundings(collectionName, `/articles/${locale.value}/${slug.value}`).order('date', 'DESC')
+  }, { server: true }
+);
+
+function formatPath(path: string): string {
+  const pathArray = path.split('/');
+  return `/${pathArray[2]}/${pathArray[1]}/${pathArray[3]}`;
+}
 </script>
 
 <template>
@@ -17,5 +30,40 @@ const { data: article } = await useLazyAsyncData(route.path, () => {
     </header>
 
     <ContentRenderer v-if="article" :value="article" class="prose dark:prose-invert max-w-full px-5" />
+
+    <div class="px-5">
+      <hr class="h-line" />
+
+      <div class="grid gap-8 sm:grid-cols-2">
+        <template v-if="surroundingArticles?.[1]">
+          <NuxtLink :to="formatPath(surroundingArticles[1].path)"
+                    class="block px-6 py-8 border rounded-lg border-gray-50! dark:border-gray-950! hover:bg-gray-50/50 dark:hover:bg-gray-950/50 group transition-colors duration-300">
+            <div class="inline-flex items-center rounded-full p-1.5 bg-gray-100 dark:bg-gray-800 ring-1 ring-gray-300 dark:ring-gray-700 mb-4">
+              <UIcon name="i-heroicons-arrow-left" class="size-5 text-gray-900 dark:text-white group-hover:text-green-500 transition-colors duration-300" />
+            </div>
+
+            <p class="font-medium text-gray-900 dark:text-white text-lg font-serif mb-1 line-clamp-1 md:line-clamp-none">
+              {{ surroundingArticles[1].title }}
+            </p>
+          </NuxtLink>
+        </template>
+
+        <div v-else class="hidden sm:block px-6 py-8">&nbsp;</div>
+
+        <template v-if="surroundingArticles?.[0]">
+          <NuxtLink :to="formatPath(surroundingArticles[0].path)" class="block px-6 py-8 border rounded-lg border-gray-50! dark:border-gray-950! hover:bg-gray-50/50 dark:hover:bg-gray-950/50 group transition-colors duration-300 text-right">
+            <div class="inline-flex items-center rounded-full p-1.5 bg-gray-100 dark:bg-gray-800 ring-1 ring-gray-300 dark:ring-gray-700 mb-4">
+              <UIcon name="i-heroicons-arrow-right" class="size-5 text-gray-900 dark:text-white group-hover:text-green-500 transition-colors duration-300" />
+            </div>
+
+            <p class="font-medium text-gray-900 dark:text-white text-lg font-serif mb-1 line-clamp-1 md:line-clamp-none">
+              {{ surroundingArticles[0].title }}
+            </p>
+          </NuxtLink>
+        </template>
+      </div>
+
+      <hr class="h-line" />
+    </div>
   </article>
 </template>
