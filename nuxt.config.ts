d405ceec06cd418e0976a56d7933be0ff89a2dc6
How Bizarre <howbizarre@gmail.com>
2026-01-04 16:37:56 +0200

    Refactor configuration files for consistency and add typecheck script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,6 +67,8 @@ export default defineNuxtConfig({
             if (id.includes('node_modules')) {
               return 'vendor';
             }
+
+            return undefined;
           }
         }
       }
